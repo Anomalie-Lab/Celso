2f408218d1382d540347614d9c804df90e945f31
Davi Freitas <davifreitaz999@gmail.com>
2025-09-02 16:37:44 -0300

    fix:(checkout):retirando h1 debaixo do header

--- client/src/app/checkout/page.tsx
+++ client/src/app/checkout/page.tsx
@@ -51,7 +51,7 @@ export default function CheckoutPage() {
       <div className="max-w-7xl mx-auto px-4 py-8 mt-6">
         {/* Header Elegante */}
         <div className="mb-12">
-          <div className="flex items-center justify-between mb-6">
+          <div className="flex items-center justify-between mb-6 mt-12">
             <div className="flex items-center gap-4">
               <div className="w-12 h-12 bg-primary rounded-full flex items-center justify-center">
                 <ShoppingBag className="w-6 h-6 text-white" />

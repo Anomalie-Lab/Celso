7d792853d9d9516b70d9a6ead9b99c375aaf477a
Davi Freitas <davifreitaz999@gmail.com>
2025-09-01 23:01:02 -0300

    fix:header causando barra de rolagem em todas as paginas

--- client/src/app/checkout/page.tsx
+++ client/src/app/checkout/page.tsx
@@ -48,7 +48,7 @@ export default function CheckoutPage() {
 
   return (
     <div className="min-h-screen bg-white p-16">
-      <div className="max-w-7xl mx-auto px-4 py-8 mt-6">
+      <div className="max-w-7xl mx-auto px-4 py-8 mt-20">
         {/* Header Elegante */}
         <div className="mb-12">
           <div className="flex items-center justify-between mb-6">

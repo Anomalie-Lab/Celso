0f27ea5697d7200a3001b312768be0716b57eb6a
Davi Freitas <davifreitaz999@gmail.com>
2025-09-01 22:04:24 -0300

    fix(fale-conosco):removendo backgroud preto da tela e deixando responsivo

--- client/src/app/fale-conosco/page.tsx
+++ client/src/app/fale-conosco/page.tsx
@@ -13,68 +13,92 @@ export default function ContactUs() {
     console.log(data);
   };
   return (
-    <main className="grid grid-cols-2 h-[800px] bg-black">
-      <section className=" w-1/2 items-center justify-center lg:pl-6 bg-red-500 ">
-      <div className="w-full">
-        <h1 className="text-3xl font-bold text-gray-900 
-        mb-4">Fale Conosco</h1>
-      </div>
+     <main className="grid grid-cols-1 lg:grid-cols-2 min-h-screen ">
+      {/* Seção Fale Conosco */}
+      <section className="w-full flex flex-col justify-start lg:justify-center lg:pl-12 p-6 lg:p-0 bg-primary text-white gap-6">
+        <h1 className="text-3xl font-bold mb-6 mt-40 lg:mt-0">Fale Conosco</h1>
 
-        <div className="max-w-[400px]">
-          <h2 className="font-bold">• E-mail</h2>
-          <p className="font-light">
-            Você também pode tirar todas suas dúvidas pelo e-mail <strong>sac@gmail.com.br</strong>
-          </p>
-          <p className="font-light">De segunda à sexta-feira, das 9 às 18 horas.</p>
+        <div className="space-y-4 ">
+          <div>
+            <h2 className="font-bold">• E-mail</h2>
+            <p className="font-light">
+              Você também pode tirar todas suas dúvidas pelo e-mail{" "}
+              <strong>sac@gmail.com.br</strong>
+            </p>
+            <p className="font-light">De segunda à sexta-feira, das 9 às 18 horas.</p>
+          </div>
+
+          <div>
+            <h2 className="font-bold">• Formulário</h2>
+            <p className="font-light">
+              Se preferir, preencha com seus dados para que possamos responder.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-bold">• Informações:</h2>
+            <p className="font-light">Atendimento de segunda a sexta das 9:00 às 18:00</p>
+          </div>
+
+          <div>
+            <h2 className="font-bold">• Medicine</h2>
+            <p className="font-light">New York, Estados Unidos - NY | CEP: 000-001</p>
+          </div>
+
+          <div>
+            <p className="font-bold">
+              TEDIO DOS SANTOS OLIVEIRA - CNPJ: 00.000.000/0001-11
+            </p>
+          </div>
+
+          <p className="text-sm mt-4">© Copyright 2025 - Todos os Direitos Reservados!</p>
         </div>
-
-        <div className="max-w-[400px]">
-          <h2 className=" font-bold">• Formulário</h2>
-          <p className="font-light">Se preferir preencha com seus dados para que possamos responder.</p>
-        </div>
-
-
-        <div className="max-w-[400px]">
-          <h2 className=" font-bold">• Informações:</h2>
-          <p className="font-light">Atendimento de segunda a sexta das 9:00 as 18:00</p>
-        </div>
-
-        <div className="max-w-[400px]">
-          <h2 className=" font-bold">• Medicine </h2>
-          <p className="font-light">new York, Estados Unidos - NY | CEP: 000-001</p>
-        </div>
-
-        <div className="max-w-[400px]">
-          <p className="font-bold">TEDIO DOS SANTOS OLIVEIRA CNPJ: 00.000.000/0001-11 </p>
-        </div>
-        <p>© Copyright 2025 - Todos os Direitos Reservados !</p>
       </section>
 
-      <section className=" w-[100%] lg:w-1/2 flex flex-col items-center justify-center lg:pl-16 mt-9">
-        <h3 className="font-medium text-[#555555] text-[18px] text-start  w-full">Enviar uma mensagem</h3>
-        <form action="" onSubmit={handleSubmit(onSubmit)} className="flex flex-col mt-4  w-full">
+      {/* Seção Formulário */}
+    <section className="w-full flex flex-col items-center justify-start lg:justify-center lg:pl-16 p-6">
+        <h3 className="font-medium text-[#555555] text-lg w-full text-center mt-40">
+          Enviar uma mensagem
+        </h3>
+
+        <form
+          action=""
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col mt-4 w-full max-w-[530px] gap-4"
+        >
           <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }}>
-            <p className="text-[13px] text-[#555555] ">Nome completo:</p>
-            <InputAuthUi {...register("name")} name="name" type="text" error={formState.errors.name?.message} className="border  w-full max-w-[530px] pl-3" />
+      
+            <InputAuthUi 
+            placeholder="Nome"
+            {...register("name")} name="name" type="text" error={formState.errors.name?.message}  />
           </motion.div>
 
           <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }}>
-            <p className="text-[13px] text-[#555555] ">endereço de email:</p>
-            <InputAuthUi {...register("email")} name="email" type="email" error={formState.errors.email?.message} className="border  w-full not-visited:pl-3 max-w-[530px]" />
-          </motion.div>
-          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }}>
-            <p className="text-[13px] text-[#555555] ">telefone</p>
-            <InputAuthUi {...register("phone")} name="phone" error={formState.errors.phone?.message} type="text" maxLength={11} className="border  w-full   pl-3 max-w-[530px]" />
+        
+            <InputAuthUi 
+             placeholder="Email"
+            {...register("email")} name="email" type="email" error={formState.errors.email?.message}  />
           </motion.div>
 
           <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }}>
-            <p className="text-[13px] text-[#555555] ">Mensagem</p>
-            <textarea {...register("txt_message")} placeholder="Mensagem" className="border p-2 rounded h-32 max-w-[530px] w-full resize-none" />
+        
+            <InputAuthUi
+             placeholder="Telefone"
+            {...register("phone")} name="phone" error={formState.errors.phone?.message} type="text" maxLength={11}  />
           </motion.div>
-          {formState.errors.txt_message?.message && <span className="text-[var(--destructive)] text-sm mt-1 2xl:text-[17px]">{formState.errors.txt_message.message}</span>}
 
-          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }} className="flex mt-4 max-w-[530px] w-full">
-            <button className="bg-[var(--background-terciary)] px-3 py-1.5 text-white cursor-pointer hover:opacity-85">Entrar em contato</button>
+          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }}>
+       
+            <textarea {...register("txt_message")} placeholder="Mensagem" className="border border-gray-200  p-2  rounded-sm h-32 w-full resize-none outline-none" />
+            {formState.errors.txt_message?.message && (
+              <span className="text-red-500 text-sm 2xl:text-base mt-1">{formState.errors.txt_message.message}</span>
+            )}
+          </motion.div>
+
+          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }} className="flex mt-4 w-full">
+            <button className="bg-primary rounded-sm px-3 py-2 text-white hover:opacity-85 cursor-pointer w-full ">
+              Entrar em contato
+            </button>
           </motion.div>
         </form>
       </section>
--- client/src/components/ui/inputs/input.auth.tsx
+++ client/src/components/ui/inputs/input.auth.tsx
@@ -17,7 +17,7 @@ export const InputAuthUi = ({ type, placeholder, className, error, ...props }: U
       >
         {placeholder} *
       </label>
-      <div className="min-h-[15px] 2xl:my-2 mt-1.5">{error && <span className="text-red-500 text-sm mt-1 2xl:text-[17px]">{error}</span>}</div>
+      <div className="min-h-[15px] 2xl:my-2 mt-1.5">{error && <span className="text-red-500 text-sm mt-1 2xl:text-base">{error}</span>}</div>
     </motion.div>
   );
 };

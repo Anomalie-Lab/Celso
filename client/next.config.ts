a031e47a68ccef9feb77b2a89a47dfb60ec12ee3
esdrasfyy <fernaando.esdras@gmail.com>
2025-09-29 06:27:48 -0300

    feat: add new CORS origin for 'i.postimg.cc' to enhance API access

--- client/next.config.ts
+++ client/next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
         protocol: 'http',
         hostname: 'localhost',
         port: '',
